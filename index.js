71e4b08f74d5cf8b07a0f0d53ac165f74ea5409c
Weiner S. Ormanji <weinerso@gmail.com>
2017-02-28 20:57:08 +0000

    Removed bootstrap script. Addedd status endpoint

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var http = require('http');
 var express = require('express');
 var sassMiddleware = require('node-sass-middleware');
 var app = express();
@@ -23,6 +24,18 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/status', function(request, response) {
+  http.get({ host: "72.55.156.247", port: 9910, path: "/stats?sid=1&json=1" }, function(resp) {
+    var body = '';
+    resp.on('data', function(d) {
+      body += d;
+    });
+    resp.on('end', function() {
+      response.json(JSON.parse(body));
+    });
+  });
 });
 
 app.listen(app.get('port'), function() {
